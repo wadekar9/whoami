328218e961bfe2f03054be792bf325e7cc1848ae
wadekar9 <npwadekar7@gmail.com>
2025-01-11 23:38:38 +0530

    feat : add testimonials cards

--- src/components/cards/TestimonialsCard.tsx
+++ src/components/cards/TestimonialsCard.tsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import Marquee from '../ui/marquee';
+import { cn } from '@/lib/utils';
+import Image from 'next/image';
+import { TESTIMONIALS } from '@/constants/testimonials';
+
+const ImageCard = ({ img, idx }: { img: string; idx : number; }) => {
+  return (
+    <figure
+      className={cn(
+        "relative cursor-pointer overflow-hidden rounded-xl",
+        "bg-gray-950/[.01] hover:bg-gray-950/[.05] flex items-center justify-center px-5"
+      )}
+    >
+      <div className="overflow-hidden">
+        <Image src={img} width={190} height={90} alt={`${idx}-image`} />
+      </div>
+    </figure>
+  );
+};
+
 
 function TestimonialsCard() {
   return (
-    <div className="col-span-2 row-span-2 bg-[#262626] flex items-center justify-center rounded-xl"></div>
+    <div className="col-span-2 row-span-2 bg-[#262626] rounded-xl overflow-hidden border border-darkslate-100 hover:border-[#4285F4]">
+      <div className="relative flex h-full w-full flex-col items-center justify-center md:shadow-xl">
+        <Marquee pauseOnHover className="[--duration:20s]">
+          {TESTIMONIALS.map((testimonial, idx) => <ImageCard key={`${testimonial.id}`} idx={idx} img={testimonial.img} />)}
+        </Marquee>
+      </div>
+    </div>
   )
 }
 
---
+++ src/constants/testimonials.ts
@@ -0,0 +1,22 @@
+export const TESTIMONIALS = [
+  {
+    id: 1,
+    img: "/clients/jerrod.png"
+  },
+  {
+    id: 2,
+    img: "/clients/sridix-logo.png"
+  },
+  {
+    id: 3,
+    img: "/clients/tech-samrajhya.png"
+  },
+  {
+    id: 4,
+    img: "/clients/tecocraft.png"
+  },
+  {
+    id: 5,
+    img: "/clients/unizoy.png"
+  }
+];
