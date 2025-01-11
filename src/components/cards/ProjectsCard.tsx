f91790491de70e4aa18825cc6c15e98e43812ae3
wadekar9 <npwadekar7@gmail.com>
2025-01-11 19:45:55 +0530

    feat : implemented project card

--- src/components/cards/ProjectsCard.tsx
+++ src/components/cards/ProjectsCard.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Title } from '../ui'
 
 function ProjectsCard() {
   return (
-    <div className="col-span-1 row-span-1 bg-[#262626] flex items-center justify-center rounded-xl"></div>
+    <div className="col-span-1 row-span-1 bg-[#262626] rounded-xl border border-darkslate-100 hover:border-[#4285F4]">
+      <a href={'/projects'} className={`h-full w-full flex items-center justify-center gap-4 hover:scale-110 transition ease-in-out duration-300`}>
+        <Title label='🛠️  Projects' />
+        <div className='float-right'>
+          <img src='./arrow-up-right.svg' className='h-6 w-6' />
+        </div>
+      </a>
+    </div>
   )
 }
 
