a91ac101e6713e61eebab9bbbf766e6f44230ad8
wadekar9 <npwadekar7@gmail.com>
2025-02-15 22:36:56 +0530

    feat: update project description and enhance icon set; conditionally render project dates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -69,13 +69,12 @@ export default function Page() {
                 <div className="inline-block rounded-lg bg-foreground text-background px-3 py-1 text-sm">
                   My Projects
                 </div>
-                <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
+                {/* <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
                   Check out my latest work
-                </h2>
+                </h2> */}
                 <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
                   I&apos;ve worked on a variety of projects, from simple
-                  websites to complex web applications. Here are a few of my
-                  favorites.
+                  apps to complex mobile applications. Here're a few of my projects.
                 </p>
               </div>
             </div>
--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,9 +1,10 @@
-import { GlobeIcon, MailIcon } from "lucide-react";
+import { GlobeIcon, MailIcon, Smartphone } from "lucide-react";
 
 export type IconProps = React.HTMLAttributes<SVGElement>;
 
 export const Icons = {
   globe: (props: IconProps) => <GlobeIcon {...props} />,
+  smartphone: (props: IconProps) => <Smartphone {...props} />,
   email: (props: IconProps) => <MailIcon {...props} />,
   linkedin: (props: IconProps) => (
     <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" {...props}>
