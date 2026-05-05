6583af4f73bcc4e166f4c24a00ffcea49cabc784
Nayan Wadekar <npwadekar7@gmail.com>
2026-05-05 12:43:51 +0530

    Update resume.tsx

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -105,7 +105,7 @@ export const DATA = {
       },
       Resume: {
         name: "Download Resume",
-        url: "https://drive.google.com/file/d/14AUAtWmYIdBJnEt_OuqWYAQdFteiQumh/view?usp=sharing",
+        url: "https://drive.google.com/file/d/14AUAtWmYIdBJnEt_OuqWYAQdFteiQumh/view?usp=drivesdk",
         icon: Icons.resumecv,
         navbar: true,
       },
